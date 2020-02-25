bd548442943ff08269c656ff7f20ea1b83980271
bochipachibuchi <shin.supernoob@gmail.com>
2020-02-25 11:12:41 +0900

    [EDIT] BookDetail Screen UI

--- src/Screens/BookDetail.js
+++ src/Screens/BookDetail.js
@@ -27,7 +27,11 @@ export default function BookDetail({
     <Container>
       <Template>
         <LeftContainer>
-          <img className="bookImage" src={String(book.image).replace("type=m1&", "")} alt={book.title} ></img>
+          <img
+            className="bookImage"
+            src={String(book.image).replace("type=m1&", "")}
+            alt={book.title}
+          ></img>
         </LeftContainer>
         <RightContainer>
           <Item>
@@ -36,14 +40,15 @@ export default function BookDetail({
           <Divider></Divider>
           <Item className="description">{book.description}</Item>
           <Item className="author-wrap">
-            <img src="https://secure.gravatar.com/avatar/f56af396b817f5a028808653642862de?s=50&amp;d=mm&amp;r=g" alt={book.author}></img>
+            <img
+              src="https://secure.gravatar.com/avatar/f56af396b817f5a028808653642862de?s=50&amp;d=mm&amp;r=g"
+              alt={book.author}
+            ></img>
             <span>{book.author}</span>
           </Item>
           <Item>{book.publisher}</Item>
         </RightContainer>
-        <ContentContainer>
-          Contents
-        </ContentContainer>
+        <ContentContainer>Contents</ContentContainer>
       </Template>
     </Container>
   );
@@ -51,16 +56,16 @@ export default function BookDetail({
 
 const Container = styled.div`
   margin: 90px auto 20px;
-  box-sizing:border-box;
+  box-sizing: border-box;
 `;
 
 const Template = styled.div`
-  position:relative;
-  margin:0 auto;
-  padding-top:30px;
+  position: relative;
+  margin: 0 auto;
+  padding-top: 30px;
 
   @media only screen and (max-width: 1200px) {
-    height:500px;
+    height: 500px;
   }
 
   @media only screen and (min-width: 1200px) {
@@ -71,67 +76,67 @@ const Template = styled.div`
 
 const LeftContainer = styled.div`
   @media only screen and (max-width: 1200px) {
-    width:100%;
+    width: 100%;
   }
 
   @media only screen and (min-width: 1200px) {
-    width:250px;
-    float:left;
+    width: 250px;
+    float: left;
   }
 
-  > img.bookImage{
-    width:100%;
+  > img.bookImage {
+    width: 100%;
   }
 `;
 
 const RightContainer = styled.div`
   @media only screen and (max-width: 1200px) {
-    width:100%;
+    width: 100%;
   }
 
   @media only screen and (min-width: 1200px) {
-    padding:0 30px;
-    width:calc(100% - 250px);
-    float:left;
+    padding: 0 30px;
+    width: calc(100% - 250px);
+    float: left;
   }
 `;
 
 const Item = styled.div`
   margin-bottom: 10px;
-  color:#333;
+  color: #333;
 
   &.description {
-    color:#555;
-    font-weight:300;
-    line-height:20px;
-    padding-right:30px;
-    margin-bottom:30px;
+    color: #555;
+    font-weight: 300;
+    line-height: 20px;
+    padding-right: 30px;
+    margin-bottom: 30px;
   }
 
   &.author-wrap {
-    display:flex;
+    display: flex;
     > img {
       width: 40px;
       height: 40px;
-      border-radius:20px;
-      margin-right:15px;
+      border-radius: 20px;
+      margin-right: 15px;
     }
     > span {
       width: calc(100% - 55px);
-      line-height:40px;
+      line-height: 40px;
     }
   }
 `;
 
 const Divider = styled.div`
-  height:1px;
-  width:100%;
-  background:#CCC;
-  margin:15px 0;
+  height: 1px;
+  width: 100%;
+  background: #ccc;
+  margin: 15px 0;
 `;
 
 const ContentContainer = styled.div`
-  margin-top:30px;
-  width:100%;
-  float:left;
+  margin-top: 30px;
+  width: 100%;
+  float: left;
 `;
