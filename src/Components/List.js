6b369be3ed80900f6ec34aac38646f4dd935ec6d
Lee Hyung Soo <mori29@gmail.com>
2020-03-02 23:32:03 +0900

    shelf 수정 중

--- src/Components/List.js
+++ src/Components/List.js
@@ -12,25 +12,48 @@ const List = ({ booklist, clickBooklist, deleteBL }) => {
     deleteBL(booklist._id);
   };
 
+  console.log(booklist);
+
   return (
     <Container>
       <Title onClick={clickEvent}>북리스트 : {booklist.title}</Title>
       {booklist.userBL ? (
         <DeleteBtn onClick={clickDelete}>삭제</DeleteBtn>
       ) : null}
+      <BookListUl>
+        {booklist.books.map((item, index) => {
+          return <BookListLi key={index}>
+            <BookImage src={String(item.image).replace("type=m1&", "type=m5&")} alt={item.image}></BookImage>
+            <div className="desc-wrap">
+              <span className="title">제목입니다~!</span>
+            </div>
+            </BookListLi>
+        })}
+      </BookListUl>
     </Container>
   );
 };
 
+const BookListUl = styled.ul`
+  display:flex;
+`;
+
+
+
+const BookListLi = styled.li`
+
+`;
+
+const BookImage = styled.div`
+  width:130px;
+  height:180px;
+  background: url(${props => props.src}) no-repeat center;
+`;
+
 const Container = styled.div`
   font-size: 1rem;
-  width: 80%;
-  height: 100px;
-  border: 1px solid black;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  width: 100%;
+  overflow:hidden;
 
   margin-bottom: 30px;
   position: relative;
