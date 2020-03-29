2efcce9ea1eadf16342bbf1e75b40040767960d5
Lee Hyung Soo <mori29@gmail.com>
2020-03-29 23:35:48 +0900

    BookDetail!!!!

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -9,23 +9,79 @@ const Comment = ({ comment, user, deleteComment, book }) => {
     deleteComment(comment.uuid);
   };
 
+  const convertDate = (date) => {
+    let year = date.getFullYear();
+    let month = date.getMonth()+1;
+    let day = date.getDate();
+
+    month = month < 10 ? "0"+month : month;
+    day = day < 10 ? "0"+day : day;
+
+    return year + "." + month + "." + day;
+  }
+
+  console.log(comment.createdAt);
+
   return (
     <Container>
-      {book !== undefined ? <span>{book.title}</span> : null}
-      <h5>작성 시간 : {new Date(comment.createdAt).toString()}</h5>
+      <div>
+        {/* {book !== undefined ? <span>{book.title}</span> : null}
+        <h5>작성 시간 : {new Date(comment.createdAt).toString()}</h5>
 
-      <h5>작성자 이름: {user.nickname}</h5>
-      <h5>댓글 내용 : {comment.description}</h5>
-      {googleUser.googleId === user.googleId ? (
-        <button onClick={clickDelete}>삭제</button>
-      ) : null}
+        <h5>작성자 이름: {user.nickname}</h5>
+        <h5>댓글 내용 : {comment.description}</h5> */}
+        
+        <div className="content">{comment.description}</div>
+        <div className="writer">
+          <span>{user.nickname}</span>
+          <span>{ convertDate(new Date(comment.createdAt))}</span>
+          {googleUser.googleId === user.googleId ? (
+            <span className="remove" onClick={clickDelete}>삭제</span>
+          ) : null}
+        </div>
+      </div>
     </Container>
   );
 };
 
-const Container = styled.div`
+const Container = styled.li`
   color: black;
   margin-top: 10px;
+
+  &:first-child{
+    border-top: 0 none;
+  }
+  padding: 10px 10px 8px;
+  border-top: 1px solid #dddfe1;
+  background-color: #fff;
+
+  > div {
+    .content {
+      overflow: hidden;
+      width: 100%;
+      font-size: 14px;
+      color: #333;
+      line-height: 20px;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
+    .writer{
+      position: relative;
+      height: 26px;
+      padding-top: 8px;
+      font-size: 12px;
+      color: #666;
+      line-height: 18px;
+      > span {
+        padding-right: 9px;
+        &.remove {
+          float: right;
+        }
+      }
+    }
+
+  }
+
 `;
 
 export default Comment;
--- src/Screens/BookDetail.js
+++ src/Screens/BookDetail.js
@@ -9,6 +9,16 @@ import reducer, { initialState, ADD, DEL } from "../Components/Reducer/reducer";
 import Comment from "../Components/Comment";
 
 import Modal from "../Components/Modal";
+import useWindowSize from "../Components/WindowSize";
+
+import Icon from "@mdi/react";
+import {
+  mdiBookPlusMultiple,    // 책장에 추가
+  mdiRead,       // 읽음
+  mdiAlphaRCircle,
+  mdiHeart,                // 좋아요
+  mdiHeartOutline
+} from "@mdi/js";
 
 export default function BookDetail({
   location: { pathname },
@@ -37,10 +47,17 @@ export default function BookDetail({
   const [averageTime, setAverageTime] = useState("읽지 않음");
   const [averageDifficulty, setAverageDifficulty] = useState("읽지 않음");
 
+  const [maxNumTime, setMaxNumTime] = useState(0);
+  const [maxNumDifficulty, setMaxNumDifficulty] = useState(0);
+  const [arrTime, setArrTime] = useState([0,0,0]);
+  const [arrDifficulty, setArrDifficulty] = useState([0,0,0]);
+  const [statisticLocation, setStatisticLocation] = useState(0);
+
   const isTokenExist = AuthApi.getToken();
 
   const googleAuth = useGoogleAuth();
   const valid = useIsValid();
+  const size = useWindowSize();
 
   const saveComment = async () => {
     const COMMENT_DATA = {
@@ -142,6 +159,27 @@ export default function BookDetail({
       setAverageTime(maxTime);
       setAverageDifficulty(maxDifficulty);
 
+      let arrTmpTime = [];
+      let maxTmpTime = 0;
+      for(const item in averageTime){
+        arrTmpTime.push(averageTime[item]);
+        if(averageTime[item] > maxTmpTime)
+          maxTmpTime = averageTime[item];
+      }
+      setArrTime(arrTmpTime);
+      setMaxNumTime(maxTmpTime);
+      
+      let arrTmpDiff = [];
+      let maxTmpDiff = 0;
+      for(const item in averageDifficulty){
+        arrTmpDiff.push(averageDifficulty[item]);
+        if(averageDifficulty[item] > maxTmpDiff)
+          maxTmpDiff = averageDifficulty[item];
+      }
+      setArrDifficulty(arrTmpDiff);
+      setMaxNumDifficulty(maxTmpTime);
+      setTimeout(statisticLocationChange, 5000);
+
       if (isTokenExist != null) {
         getUserBooklist(user);
         getReadLogger(user._id, Results._id);
@@ -156,6 +194,11 @@ export default function BookDetail({
       alert(err);
     }
   };
+
+  const statisticLocationChange = () => {
+    if(statisticLocation === 0) setStatisticLocation(1);
+    else setStatisticLocation(0);
+  }
 
   const deleteComments = async commentId => {
     try {
@@ -255,14 +298,48 @@ export default function BookDetail({
     showBook(authorized);
   };
 
+  const actionButtons = isTokenExist !== null ? (
+    <ButtonTemplate>
+      <AddBookBtn
+        doneReading={doneReading}
+        onClick={() => {
+          if (doneReading) {
+            cancelReading();
+            setReadNum(prev => prev - 1);
+          } else {
+            clickReadBook();
+          }
+        }}
+      >
+        <Icon path={doneReading ? mdiAlphaRCircle : mdiRead } size={size.width >= 768 ? `14px` : `16px`} color={`#666`}></Icon>
+        <span>{readNum}</span>
+      </AddBookBtn>
+      <AddBookBtn
+        wish={wish}
+        onClick={() => {
+          clickWishlist(wish);
+        }}
+      >
+        <Icon path={wish ? mdiHeart : mdiHeartOutline} size={size.width >= 768 ? `14px` : `16px`} color={`red`}></Icon>
+        <span>{wishNum}</span>
+      </AddBookBtn>
+      <AddBookBtn onClick={clickAddBook}> <Icon path={mdiBookPlusMultiple} size={size.width >= 768 ? `14px` : `16px`} color={`#666`}></Icon><span>책장에 추가</span></AddBookBtn>
+    </ButtonTemplate>
+  ) : null;
+
   useEffect(() => {
     getUser();
+    window.scrollTo(0, 0);
   }, []);
+
+  useEffect(() => {
+    setTimeout(statisticLocationChange, 5000);
+  }, [statisticLocation]);
 
   return (
     <>
       <Container>
-        <LeftContainer>
+        <LeftContainer image={String(book.image).replace("type=m1&", "")} >
           <img
             className="bookImage"
             src={String(book.image).replace("type=m1&", "")}
@@ -272,64 +349,98 @@ export default function BookDetail({
         <RightContainer>
           <Item>
             <h1>{book.title}</h1>by {book.author}
+            {size.width >= 768 && actionButtons}
           </Item>
           <Divider></Divider>
           <Item className="description">{book.description}</Item>
-          <Item className="author-wrap">
-            <img
-              src="https://secure.gravatar.com/avatar/f56af396b817f5a028808653642862de?s=50&amp;d=mm&amp;r=g"
-              alt={book.author}
-            ></img>
-            <span>{book.author}</span>
-          </Item>
-          <Item>{book.publisher}</Item>
-          <Item> 평균 읽은 시간 : {averageTime}</Item>
-          <Item> 평균 난이도 : {averageDifficulty}</Item>
-          <Item>카테고리 : {book.category}</Item>
-          {isTokenExist !== null ? (
-            <ButtonTemplate>
-              <AddBookBtn onClick={clickAddBook}>책장에 추가</AddBookBtn>
-
-              <AddBookBtn
-                doneReading={doneReading}
-                onClick={() => {
-                  if (doneReading) {
-                    cancelReading();
-                    setReadNum(prev => prev - 1);
-                  } else {
-                    clickReadBook();
-                  }
-                }}
-              >
-                읽음 : {readNum}
-              </AddBookBtn>
-              <AddBookBtn
-                wish={wish}
-                onClick={() => {
-                  clickWishlist(wish);
-                }}
-              >
-                좋아요 # : {wishNum}
-              </AddBookBtn>
-            </ButtonTemplate>
-          ) : null}
+          <TableWrap>
+            <Table>
+              <TableLeft>작가/저자</TableLeft>
+              <TableRight>{book.author}</TableRight>
+            </Table>
+            <Table>
+              <TableLeft>출판사</TableLeft>
+              <TableRight>{book.publisher}</TableRight>
+            </Table>
+            <Table>
+              <TableLeft>카테고리</TableLeft>
+              <TableRight>{book.category}</TableRight>
+            </Table>
+            <StatisticWrap>
+              <Statistic max={maxNumTime} arr={arrTime} location={statisticLocation} className={statisticLocation === 0 ? `` : `hidden`}>
+                <div className="title">이 책을 읽은 사람의 소요 시간</div>
+                <Divider className="sub"/>
+                <ul>
+                  <li>
+                    <span className="type">1주 이내</span>
+                    <span className="value-wrap">
+                      <span className="value type0"></span>
+                      {arrTime[0]}명
+                    </span>
+                  </li>
+                  <li>
+                    <span className="type">1개월 이내</span>
+                    <span className="value-wrap">
+                      <span className="value type1"></span>
+                      {arrTime[1]}명
+                    </span>
+                  </li>
+                  <li>
+                    <span className="type">1개월 이상</span>
+                    <span className="value-wrap">
+                      <span className="value type2"></span>
+                      {arrTime[2]}명
+                    </span>
+                  </li>
+                </ul>
+              </Statistic>
+              <Statistic max={maxNumDifficulty} arr={arrDifficulty} location={statisticLocation} className={statisticLocation === 0 ? `hidden` : ``} >
+                <div className="title">이 책을 읽은 사람이 느끼는 난이도</div>
+                <Divider className="sub"/>
+                <ul>
+                  <li>
+                    <span className="type">쉬움</span>
+                    <span className="value-wrap">
+                      <span className="value type0"></span>
+                      {arrDifficulty[0]}명
+                    </span>
+                  </li>
+                  <li>
+                    <span className="type">보통</span>
+                    <span className="value-wrap">
+                      <span className="value type1"></span>
+                      {arrDifficulty[1]}명
+                    </span>
+                  </li>
+                  <li>
+                    <span className="type">어려움</span>
+                    <span className="value-wrap">
+                      <span className="value type2"></span>
+                      {arrDifficulty[2]}명
+                    </span>
+                  </li>
+                </ul>
+              </Statistic>
+            </StatisticWrap>
+            {size.width < 768 && actionButtons}
+          </TableWrap>
+          
         </RightContainer>
         <ContentContainer>
           <CommentCotainer>
             <CommentForm onSubmit={onSubmit}>
-              <ComentTitle />
+              <ComentTitle>댓글쓰기</ComentTitle>
               <CommentSection
-                placeholder="댓글 입력"
+                placeholder="댓글을 입력해주세요"
                 value={commentText}
                 onChange={onChange}
               />
               <CommentSubmit type="submit">등록</CommentSubmit>
             </CommentForm>
-            <Dividers />
-            <CommentTitle>댓글</CommentTitle>
-            <Dividers />
+            <br/><br/><br/>
+            <CommentTitle>댓글 <span>{allComment.length}개</span></CommentTitle>
+            <Divider></Divider>
             <CommentList>
-              <h4>추가된 댓글들 </h4>
               {allComment.map(comment => (
                 <Comment
                   key={comment._id}
@@ -428,16 +539,46 @@ export default function BookDetail({
 const ButtonTemplate = styled.div`
   display: flex;
   justify-content: center;
+  @media only screen and (max-width: 767px) {
+    margin-top:15px;
+  }
+  @media only screen and (min-width: 768px) {
+    position:absolute;
+    bottom:-5px;
+    right:0;
+  }
 `;
 
 const AddBookBtn = styled.button`
-  border: 1px solid black;
-  width: 100px;
-  height: 50px;
-  padding: 5px;
+  display:flex;
+  border: 1px solid #CCC;
+  border-radius: 15px;
+  
   text-align: center;
+  margin-right:5px;
   background-color: ${props =>
-    props.wish ? "blue" : props.doneReading ? "blue" : "white"};
+    props.wish ? "#EEE" : props.doneReading ? "#EEE" : "#FFF"};
+  &:hover{
+    background-color:#DDD;
+  }
+  > span {
+    margin-left: 5px;
+    color:#666;
+    font-weight:500;
+  }
+
+  @media only screen and (max-width: 767px) {
+    padding: 8px 16px;
+    > span {
+      font-size: 12px;
+    }
+  }
+  @media only screen and (min-width: 768px) {
+    padding: 5px 10px;
+    > span {
+      font-size: 11px;
+    }
+  }
 `;
 
 const CloseBtn = styled.span`
@@ -471,22 +612,61 @@ const Container = styled.div``;
 
 const LeftContainer = styled.div`
   @media only screen and (max-width: 767px) {
-    width: 100%;
+    position: absolute;
+    width: calc(100% + 20px);
+    height: 300px;
+    top:0;
+    left:-10px;
+    right:-10px;
+    overflow:hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    > img.bookImage {
+      display:block;
+      width: 100%;
+      max-width: 200px;
+      margin: 0 auto;
+      border-top: solid 1px rgba(255,255,255,0.05);
+      border-left: solid 1px rgba(255,255,255,0.05);
+      box-shadow: 5px 5px 5px rgba(0,0,0,0.2);
+      max-height: 280px;
+      z-index: 10;
+      margin-bottom:5px;
+    }
+
+    &:before {
+      background: Url(${props => (props.image)}) center;
+      background-size: calc(100% + 20px);
+      content: "";
+      position:absolute;
+      left:-10px;
+      right:-10px;
+      top:-10px;
+      bottom:-10px;
+      width:calc(100% + 20px);
+      height:320px;
+      filter: blur(0.3rem);
+      opacity:0.5;
+    }
   }
 
   @media only screen and (min-width: 768px) {
     width: 250px;
     float: left;
-  }
 
-  > img.bookImage {
-    width: 100%;
+    > img.bookImage {
+      width: 100%;
+    }
+
   }
 `;
 
 const RightContainer = styled.div`
   @media only screen and (max-width: 767px) {
     width: 100%;
+    margin-top: 280px;
   }
 
   @media only screen and (min-width: 768px) {
@@ -500,29 +680,62 @@ const RightContainer = styled.div`
   }
 `;
 
+const TableWrap = styled.div`
+  position:relative;
+  overflow:hidden;
+`;
+
+const Table = styled.div`
+  width: 100%;
+  display:table;
+`;
+
+const TableLeft = styled.div`
+  display: table-cell;
+  padding: 5px 0;
+  width: 70px;
+  line-height: 16px;
+  vertical-align: top;
+  font-size: 14px;
+  color: #999;
+  text-align: left;
+  white-space: nowrap;
+`;
+
+const TableRight = styled.div`
+  display: table-cell;
+  padding: 5px 0;
+  line-height: 16px;
+  vertical-align: top;
+  font-size: 13px;
+  color: #666;
+  text-align: left;
+`;
+
 const Item = styled.div`
   margin-bottom: 10px;
   color: #333;
+  position:relative;
 
-  &.description {
-    color: #555;
-    font-weight: 300;
-    line-height: 20px;
-    padding-right: 30px;
-    margin-bottom: 30px;
+  @media only screen and (max-width: 767px) {
+    h1 {
+      font-size: 22px;
+    }
+    &.description {
+      color: #555;
+      font-weight: 300;
+      line-height: 20px;
+      margin-bottom: 30px;
+    }
   }
 
-  &.author-wrap {
-    display: flex;
-    > img {
-      width: 40px;
-      height: 40px;
-      border-radius: 20px;
-      margin-right: 15px;
-    }
-    > span {
-      width: calc(100% - 55px);
-      line-height: 40px;
+  @media only screen and (min-width: 768px) {
+    &.description {
+      color: #555;
+      font-weight: 300;
+      line-height: 20px;
+      padding-right: 30px;
+      margin-bottom: 30px;
     }
   }
 `;
@@ -531,76 +744,246 @@ const Divider = styled.div`
   height: 1px;
   width: 100%;
   background: #ccc;
-  margin: 15px 0;
+  
+  @media only screen and (max-width: 767px) {
+    margin: 10px 0;
+  }
+  @media only screen and (min-width: 768px) {
+    margin: 15px 0;
+  }
+  
+  &.sub{
+    margin-top:3px;
+    margin-bottom:5px;
+  }
+  
+`;
+
+const StatisticWrap = styled.div`
+  @media only screen and (max-width: 767px) {
+    margin-top:15px;
+    width:200%;
+    height:78px;
+    overflow:hidden;
+  }
+
+  @media only screen and (min-width: 768px) {
+    position:absolute;
+    right:0;
+    bottom:0;
+    width:260px;
+    height:69px;
+    overflow:hidden;
+  }
+`;
+
+const Statistic = styled.div`
+  @media only screen and (max-width: 767px) {
+    display:inline-block;
+    width:50%;
+    margin:0 auto;
+    transform: translateX(-${props => props.location === 0 ? "0" : "100"}%);
+    //transition: opacity 2s;
+    transition: 1s;
+    opacity:1;
+    &.hidden{
+      opacity:0;
+    }
+
+    .title {
+      display:inline-block;
+      color:#333;
+      //background:#da3e58;
+      font-size: 14px;
+      font-weight: 600;
+      padding-left:10px;
+    }
+    ul {
+      margin-top: 5px;
+      width: 300px;
+      li{
+        display:flex;
+        margin-botton:3px;
+        .type{
+          display:inline-block;
+          width:100px;
+          color:#666;
+          font-size: 12px;
+          font-weight:400;
+          padding-left:10px;
+        }
+        .value-wrap{
+          display:inline-block;
+          color:#666;
+          font-size: 10px;
+          font-weight: 400;
+          height: 17px;
+          width: 200px;
+          .value{
+            display:inline-block;
+            height: 8px;
+            padding-right:5px;
+            margin-right:5px;
+            border-radius: 0 5px 5px 0;
+            margin-top:2px;
+            &.type0 {
+              width: ${props => (props.arr[0]/props.max)*160}px;
+              background:#3b94c7;
+            }
+            &.type1 {
+              width: ${props => (props.arr[1]/props.max)*160}px;
+              background:#a70b9b;
+            }
+            &.type2 {
+              width: ${props => (props.arr[2]/props.max)*160}px;
+              background:#1ab907;
+            }
+          }
+        }
+      }
+    }
+  }
+  @media only screen and (min-width: 768px) {
+    display:inline-block;
+    transform: translateY(-${props => props.location === 0 ? "0" : "69"}px);
+    //transition: opacity 2s;
+    transition: 1s;
+    opacity:1;
+    &.hidden{
+      opacity:0;
+    }
+
+    .title {
+      display:inline-block;
+      color:#333;
+      //background:#da3e58;
+      font-size: 12px;
+      font-weight: 600;
+    }
+    ul {
+      margin-top: 5px;
+      width: 260px;
+      li{
+        display:flex;
+        margin-botton:3px;
+        .type{
+          display:inline-block;
+          width:60px;
+          color:#666;
+          font-size: 11px;
+          font-weight:400;
+        }
+        .value-wrap{
+          display:inline-block;
+          color:#666;
+          font-size: 10px;
+          font-weight: 400;
+          height: 14px;
+          width: 200px;
+          .value{
+            display:inline-block;
+            height: 8px;
+            padding-right:5px;
+            margin-right:5px;
+            border-radius: 0 5px 5px 0;
+            &.type0 {
+              width: ${props => (props.arr[0]/props.max)*160}px;
+              background:#3b94c7;
+            }
+            &.type1 {
+              width: ${props => (props.arr[1]/props.max)*160}px;
+              background:#a70b9b;
+            }
+            &.type2 {
+              width: ${props => (props.arr[2]/props.max)*160}px;
+              background:#1ab907;
+            }
+          }
+        }
+      }
+    }
+  }
+  
 `;
 
 const ContentContainer = styled.div`
-  margin-top: 30px;
-  width: 100%;
-  float: left;
+  
+
+  @media only screen and (max-width: 767px) {
+    margin-top:15px;
+    width: 100%;
+  }
+
+  @media only screen and (min-width: 768px) {
+    margin-top: 30px;
+    width: 100%;
+    float: left;
+  }
+
 `;
 
 const CommentCotainer = styled.div`
   width: 100%;
-  height: 300px;
-
   margin-bottom: 30px;
 `;
 
 const CommentForm = styled.form`
-  width: 90%;
-  height: 200px;
-
-  margin: 20px auto 0 auto;
-
-  position: relative;
+  width: 100%;
+  &:after{
+    content: "";
+    clear:both;
+    display:block;
+  }
 `;
 
 const ComentTitle = styled.span`
   display: block;
+  height:40px;
+  width: 60px;
+  line-height:40px;
+  color:#333;
+  font-size:14px;
   font-weight: 600;
-  margin-top: 20px;
-  margin-left: 40px;
-
-  :before {
-    content: "댓글쓰기";
-  }
+  text-align:center;
 `;
 
 const CommentSection = styled.textarea`
   display: block;
-  width: 90%;
-  height: 100px;
-  margin: 10px auto 0 auto;
+  padding:10px;
+  width: 100%;
+  border:1px solid #DDD;
+  height:100px;
 `;
 
 const CommentSubmit = styled.button`
-  width: 50px;
-  height: 20px;
-
-  right: 5%;
-  bottom: 10%;
-  position: absolute;
-
-  text-align: center;
+  margin-top:5px;
+  padding:8px 20px;
+  text-align:center;
+  color:#FFF;
+  float:right;
+  font-size:13px;
+  font-weight:600;
+  background:#da3e58;
+  &:hover{
+    opacity:0.8;
+  }
+  border-radius:15px;
 `;
 
 const CommentList = styled.ul`
+  margin-top: -10px;
   list-style: none;
+  letter-spacing: -0.7px;
 `;
 
 const CommentTitle = styled.span`
-  font-size: 1rem;
-  font-weight: 700;
-
-  margin-left: 5%;
-`;
-
-const Dividers = styled.div`
-  width: 100%;
-  border: 0.8px solid black;
-  margin-bottom: 5px;
-  margin-top: 5px;
+  display: block;
+  color: #333;
+  font-size: 14px;
+  font-weight: 600;
+  > span {
+    font-weight: 400;
+  }
 `;
 
 const Box = styled.div``;
