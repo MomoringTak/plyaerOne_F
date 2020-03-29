9880d214e2a179b5c0c39a554e5cfee95695cb87
bochipachibuchi <shin.supernoob@gmail.com>
2020-03-29 17:00:44 +0900

    Updates

---
+++ precache-manifest.516c7602250a19dd198eed2907fa029d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73de4b2ac3f369fad3b6b5cacca20301",
+    "url": "/WHAT_THE_BOOK/index.html"
+  },
+  {
+    "revision": "84dfcf85832f2a3e0837",
+    "url": "/WHAT_THE_BOOK/static/css/2.5808ecb8.chunk.css"
+  },
+  {
+    "revision": "1ae598e8108c33425bc9",
+    "url": "/WHAT_THE_BOOK/static/css/main.6ba8ea9e.chunk.css"
+  },
+  {
+    "revision": "84dfcf85832f2a3e0837",
+    "url": "/WHAT_THE_BOOK/static/js/2.2269d191.chunk.js"
+  },
+  {
+    "revision": "a29ac822d5cbd8e43d0b9e6f98d92ce6",
+    "url": "/WHAT_THE_BOOK/static/js/2.2269d191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae598e8108c33425bc9",
+    "url": "/WHAT_THE_BOOK/static/js/main.6403669f.chunk.js"
+  },
+  {
+    "revision": "dc5455c0f8410d51d444",
+    "url": "/WHAT_THE_BOOK/static/js/runtime-main.2edfccaa.js"
+  }
+]);
