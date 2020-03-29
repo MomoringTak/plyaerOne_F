09412812f8cbfe3c0e3aa7c60d116a543763f440
bochipachibuchi <shin.supernoob@gmail.com>
2020-03-29 17:12:12 +0900

    [CONFIGURE] DEPLOYMENT

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8008"
+  baseURL: "https://floating-earth-10094.herokuapp.com/"
 });
 
 export const searchApi = {
